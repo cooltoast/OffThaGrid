0e47054e8decfef50cf86befd66ad205dcb7497e
cooltoast <mpatel678@gmail.com>
2014-12-23 22:39:38 -0500

    fixed very confusing syntaxerror in lunch/tests.py

--- lunch/tests.py
+++ lunch/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
 from lunch.models import Event,Vendor
 from datetime import datetime
 
@@ -7,17 +9,19 @@ class EventTestCase(TestCase):
   def setUp(self):
     now = timezone.now()
     today = datetime(now.year, now.month, now.day)
-    Event.objects.create(name="todayEvent", date=today
-    Event.objects.create(name="30daysagoEvent", date=today-timedelta(days=30))
-    Event.objects.create(name="31daysagoEvent", date=today-timedelta(days=31))
+    Event.objects.create(name="todayEvent", date=today)
+    Event.objects.create(name="thirtydaysago", date=today-timedelta(days=30))
+    Event.objects.create(name="thirtyonedaysago", date=today-timedelta(days=31))
 
   def test30dayFilter(self):
     todayEvent = Event.objects.get(name="todayEvent")
-    30daysagoEvent = Event.objects.get(name="30daysagoEvent")
-    31daysagoEvent = Event.objects.get(name="31daysagoEvent")
+    thirtydaysago = Event.objects.get(name="thirtydaysago")
+    thirtyonedaysago = Event.objects.get(name="thirtyonedaysago")
 
+    now = timezone.now()
+    today = datetime(now.year, now.month, now.day)
     thirtyDaysAgo = today - timedelta(days=30)
     events = Event.objects.filter(date__gte=thirtyDaysAgo)
     self.assertTrue(todayEvent in events)
-    self.assertTrue(30daysagoEvent in events)
-    self.assertFalse(31daysagoEvent in events)
+    self.assertTrue(thirtydaysago in events)
+    self.assertFalse(thirtyonedaysago in events)
